395b46344b1dc7ff7f64da4f04c01b5593437f4e
Abed Arrahman Alhennawi <you@example.com>
2024-11-09 18:47:01 +0300

    s

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-import time
 import collections
+import time
 
+import pandas as pd
+import matplotlib.pyplot as plt
 import plib
 from icecream import ic
 
-with open('Modern Talking.txt', 'r') as f:
+with open('Modern Talking.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
+# text += text
 
-def pref(fun, *args):
+
+def pref0(fun, *args):
     start = time.perf_counter()
     fun(*args)
+    end = time.perf_counter()
+    return end - start
+
+
+def pref1(fun, *args):
+    start = time.perf_counter()
+    fun(args[0].split())
     end = time.perf_counter()
     return end - start
 
@@ -22,17 +33,40 @@ def py_counter(words):
     return rizz
 
 
-lp = []
-lc = []
+list_threads = []
+list_par = []
+list_rust = []
+list_collection = []
+for i in range(1, 60):
+    txt = text[:50_000 * i]
+    list_threads.append(pref0(plib.thread_counter, txt))
+    list_par.append(pref0(plib.par_counter, txt))
+    list_rust.append(pref0(plib.counter, txt))
+    list_collection.append(pref1(collections.Counter, txt))
 
-for i in range(10):
-    lc.append(pref(collections.Counter, text.split()))
-    lp.append(pref(plib.par_counter, text))
 
-tc = ic(sum(lc)/len(lc))
-tp = ic(sum(lp)/len(lp))
-ic(tc / tp)
-plib.show("Majd")
+plt.plot(list_collection)
+plt.plot(list_rust)
+plt.plot(list_par)
+plt.plot(list_threads)
+langs = ['Collections', 'Rust', 'Rayon', 'Rust Threads']
+plt.legend(langs)
+plt.show()
+#
+# d = dict(zip(langs, [list_collection, list_rust, list_par, list_threads]))
+# df = pd.DataFrame(d)
+#
+# print("Mean\n", df.mean(), sep="")
+# print("STD\n", df.std(), sep="")
+
+# ic(plib.thread_counter(text)['jesus'])
+# ic(plib.par_counter(text)['jesus'])
+# ic(plib.counter(text)['jesus'])
+# ic(collections.Counter(text.split())['jesus'])
+x = plib.thread_counter(text)
+# for k, v in collections.Counter(text.split()).items():
+#     if (i := x.get(k, 0)) != v:
+#         ic(k, v, i)
 
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,31 @@
+use std::sync::{Arc, Mutex};
 use pyo3::{wrap_pyfunction, prelude::*};
 use rayon::prelude::*;
 use std::collections::HashMap;
+use std::thread;
+use pyo3::types::PyDict;
 
 #[pyfunction]
-fn counter<'a>(inp: &'a str) -> HashMap<&'a str, usize>{
+fn counter<'a>(inp: &'a str) -> HashMap<&'a str, usize> {
     let mut rizz: HashMap<&str, usize> = HashMap::new();
-    for item in inp.split(" "){
+    for item in inp.split_ascii_whitespace() {
         *rizz.entry(item).or_insert(0) += 1;
     }
     rizz
 }
+
 #[pyfunction]
+<<<<<<< HEAD
 fn par_counter<'a>(inp: &'a str) -> HashMap<&'a str, usize>{
+=======
+fn par_counter<'a>(inp: &'a str) -> HashMap<&'a str, usize> {
+>>>>>>> 0656289 (s)
     inp
         .split_ascii_whitespace()
         .collect::<Vec<_>>()
         .par_iter()
-        .fold(HashMap::new, |mut acc, item| {
-            *acc.entry(*item).or_insert(0) += 1;
+        .fold(HashMap::new, |mut acc, &item| {
+            *acc.entry(item).or_insert(0) += 1;
             acc
         })
         .reduce(HashMap::new, |mut acc, item| {
@@ -28,9 +36,68 @@ fn par_counter<'a>(inp: &'a str) -> HashMap<&'a str, usize>{
         })
 }
 
+fn words_counter(text: &str) -> HashMap<&str, usize> {
+    let mut word_counts = HashMap::new();
+    for word in text.split_whitespace() {
+        *word_counts.entry(word).or_insert(0) += 1;
+    }
+    word_counts
+}
+
+#[pyfunction]
+fn thread_counter(text: String) -> PyResult<Py<PyAny>> {
+    let num_threads = 12;
+    let text_len = text.len();
+    let chunk_size = text_len / num_threads;
+
+    let word_count = Arc::new(Mutex::new(HashMap::new()));
+    let mut handles = vec![];
+
+    for i in 0..num_threads {
+        let start = i * chunk_size;
+        let end = if i == num_threads - 1 {
+            text_len
+        } else {
+            (i + 1) * chunk_size
+        };
+
+        let end = text[start..end].rfind(|c: char| c.is_whitespace()).map_or(end, |pos| start + pos);
+        let chunk = &text[start..end];
+        let word_count = Arc::clone(&word_count);
+
+        let handle = thread::spawn(move || {
+            let counts = words_counter(&chunk);
+            let mut word_count = word_count.lock().unwrap();
+            for (word, count) in counts {
+                *word_count.entry(word).or_insert(0) += count;
+            }
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    let word_count = Arc::try_unwrap(word_count).unwrap().into_inner().unwrap();
+
+    Python::with_gil(|py| {
+        let py_dict = PyDict::new(py);
+        for (word, count) in word_count {
+            py_dict.set_item(word, count)?;
+        }
+        Ok(py_dict.into())
+    })
+}
+
 #[pymodule]
-fn plib(m: &Bound<'_, PyModule>) -> PyResult<()>{
+fn plib(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(counter, m)?)?;
     m.add_function(wrap_pyfunction!(par_counter, m)?)?;
+<<<<<<< HEAD
+=======
+    m.add_function(wrap_pyfunction!(thread_counter, m)?)?;
+>>>>>>> 0656289 (s)
     Ok(())
 }
